b3ae44a7a50aafa1698bedf8d2d4d19815881c7d
aarmam <mart.aarma@gmail.com>
2019-05-30 14:49:52 +0300

    SIGARIA-109 Ignite serialization fix

--- siga-common/src/main/java/ee.openeid.siga.common/session/DataToSignHolder.java
+++ siga-common/src/main/java/ee.openeid.siga.common/session/DataToSignHolder.java
@@ -3,13 +3,35 @@ package ee.openeid.siga.common.session;
 import ee.openeid.siga.common.SigningType;
 import lombok.Builder;
 import lombok.Data;
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.SerializationUtils;
+import org.apache.ignite.binary.BinaryObjectException;
+import org.apache.ignite.binary.BinaryReader;
+import org.apache.ignite.binary.BinaryWriter;
+import org.apache.ignite.binary.Binarylizable;
 import org.digidoc4j.DataToSign;
 
 @Data
 @Builder
-public class DataToSignHolder {
+public class DataToSignHolder implements Binarylizable {
 
     private String sessionCode;
     private DataToSign dataToSign;
     private SigningType signingType;
+
+    @Override
+    @SneakyThrows
+    public void writeBinary(BinaryWriter writer) throws BinaryObjectException {
+        writer.writeString("sessionCode", sessionCode);
+        writer.writeByteArray("dataToSignSerialized", SerializationUtils.serialize(dataToSign));
+        writer.writeObject("signingType", signingType);
+    }
+
+    @Override
+    @SneakyThrows
+    public void readBinary(BinaryReader reader) throws BinaryObjectException {
+        sessionCode = reader.readString("sessionCode");
+        dataToSign = SerializationUtils.deserialize(reader.readByteArray("dataToSignSerialized"));
+        signingType = reader.readObject("signingType");
+    }
 }
