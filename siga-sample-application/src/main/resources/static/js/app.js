10358f0cc6a5a92a4949266a256de5e81e9950fc
Priit Üksküla <priit.ykskyla@nortal.com>
2019-11-06 15:14:22 +0200

    MAINTENANCE bump axios version in sample application js

--- siga-sample-application/src/main/resources/static/js/app.js
+++ siga-sample-application/src/main/resources/static/js/app.js
@@ -4,7 +4,7 @@ requirejs.config({
         "vue": "https://cdn.jsdelivr.net/npm/require-vuejs@1.1.3/dist/require-vuejs.min",
         "vue-dropzone": "/webjars/vue2-dropzone/dist/vue2Dropzone",
         "vue-bootstrap": "/webjars/bootstrap-vue/dist/bootstrap-vue.min",
-        "axios": "/webjars/axios/0.18.0/dist/axios"
+        "axios": "/webjars/axios/0.19.0/dist/axios"
     },
     shim: {
         "Vue": {"exports": "Vue"}
