c25b9040e18e8c683b23b4769f388634a7a44346
siimsuu <siim.suu@nortal.com>
2020-10-08 17:54:28 +0300

    SIGA-261 docker fixes

--- siga-webapp/src/main/resources/docker/setenv.sh
+++ siga-webapp/src/main/resources/docker/setenv.sh
@@ -1,1 +1,2 @@
 export JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=digidoc4jTest,datafileContainer,smartId,mobileId"
+export JAVA_OPTS="$JAVA_OPTS -Dspring.config.location=file:/usr/local/tomcat/conf/application.properties"
